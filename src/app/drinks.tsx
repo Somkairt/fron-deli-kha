6b1b646e2848995c720c6914d8d582edcc565449
Tiwthawat Somaumchan <liew8444@gmail.com>
2023-12-07 18:34:17 +0700

    feat: compo2

---
+++ src/app/drinks.tsx
@@ -0,0 +1,25 @@
+const drinks = [
+    {
+        imageSrc: "/bubble.jpg",
+        name: "Bubble Milk Tea",
+        price: 45,
+    },
+    {
+        imageSrc: "/coffee.jpg",
+        name: "Iced Coffee",
+        price: 50,
+    },
+    {
+        imageSrc: "/chocco.jpg",
+        name: "Hot Chocolate",
+        price: 55,
+    },
+    {
+        imageSrc: "/latte.jpg",
+        name: "Hot Latte Art",
+        price: 70,
+    },
+    // เพิ่มรายการเครื่องดื่มเพิ่มเติมตามต้องการ
+];
+
+export default drinks;
---
+++ src/app/items.tsx
@@ -0,0 +1,24 @@
+const items = [
+    { imageSrc: "/cerry.jpg", name: "Spicy Chicken Curry", price: 50 },
+    { imageSrc: "/veggy.jpg", name: "Vegetable Stir-Fry", price: 40 },
+    {
+        imageSrc: "/monmon.jpg",
+        name: "Grilled Salmon with Butter",
+        price: 60,
+    },
+    { imageSrc: "/pepper.jpg", name: "Pepperoni Pizza", price: 80 },
+    { imageSrc: "/caprese.jpg", name: "Caprese Sandwich", price: 55 },
+    { imageSrc: "/padthai.jpg", name: "Shrimp Pad Thai", price: 65 },
+    { imageSrc: "/flat.jpg", name: "Margherita Flatbread", price: 75 },
+    {
+        imageSrc: "/lasagna.jpg",
+        name: "Roasted Vegetable Lasagna",
+        price: 85,
+    },
+    { imageSrc: "/pastack.jpg", name: "Chicken Alfredo Pasta", price: 50 },
+    { imageSrc: "/somtam.jpg", name: "Papaya Salad", price: 75 },
+    { imageSrc: "/tomyam.jpg", name: "Tom Yam Kung", price: 50 },
+    { imageSrc: "/massa.jpg", name: "Massaman Majesty", price: 40 },
+];
+
+export default items;
